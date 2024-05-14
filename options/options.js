2321478215999dbf5b58acbea0fda7c447043a14
Crowdbotics <team@crowdbotics.com>
2024-05-14 17:46:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-limit-47925",
+  url: "https://square-limit-47925.botics.co",
+  api: "https://square-limit-47925.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
